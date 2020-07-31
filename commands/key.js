4b79a769dc2e69f81994e07c0a254a8ed2957140
NES <nspry1997@gmail.com>
2020-07-30 21:02:11 -0400

    method skeleton added

---
+++ commands/key.js
@@ -0,0 +1,30 @@
+const inquirer = require('inquirer')
+const colors = require('colors')
+const KeyManager = require('../lib/KeyManager')
+
+const key = {
+  async set() {
+    const keyManager = new KeyManager()
+    const input = await inquirer.prompt([
+      {
+        type: 'input',
+        name: 'key',
+        message: 'Enter API Key '.green + 'https://nomics.com'
+      }
+    ])
+
+    const key = keyManager.setKey(input.key)
+
+    if(key) {
+      console.log('API Key Set'.blue)
+    }
+  },
+  show() {
+    console.log('Show says hello')
+  },
+  remove() {
+    console.log('Remove says hello')
+  },
+}
+
+module.exports = key
